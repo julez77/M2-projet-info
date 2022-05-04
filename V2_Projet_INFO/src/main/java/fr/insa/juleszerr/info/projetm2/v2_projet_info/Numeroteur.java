a04c38fae689090543c752056b4e3339b6773102
eguy01 <eguy01@sec-c210-f09.csip.in>
2022-05-04 14:47:12 +0200

    pessi

---
+++ V2_Projet_INFO/src/main/java/fr/insa/juleszerr/info/projetm2/v2_projet_info/Numeroteur.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.juleszerr.info.projetm2.v2_projet_info;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+/**
+ *
+ * @author eguy01
+ */
+public class Numeroteur<TO> {
+  private TreeMap<Integer,TO> idVersObjet;
+    private Map<TO,Integer> objetVersId;
+    
+    private int prochainID;
+    
+    public Numeroteur() {
+        this(0);
+    }
+    
+    private Numeroteur(int prochainID) {
+        this.prochainID = prochainID;
+        this.idVersObjet = new TreeMap<>();
+        this.objetVersId = new HashMap<>();
+    }
+    
+    public int creeID(TO obj) {
+        if(this.objetVersId.containsKey(obj)) {
+            throw new Error("objet " + obj + " déjà dans le numéroteur");
+        }
+        this.idVersObjet.put(this.prochainID, obj);
+        this.objetVersId.put(obj, this.prochainID);
+        this.prochainID ++;
+        return this.prochainID - 1;
+    }
+    
+    public boolean objExist(TO obj) {
+        return this.objetVersId.containsKey(obj);
+    }
+    
+    public int getID(TO obj) {
+        if (this.objExist(obj)) {
+            return this.objetVersId.get(obj);
+        } else {
+            throw new Error("Objet" + obj + " inconnu dans numéroteur");
+        }
+    }
+
+    public int getOuCreeID(TO obj) {
+        if (this.objExist(obj)) {
+            return this.objetVersId.get(obj);
+        } else {
+            return this.creeID(obj);
+        }
+    }
+    
+    public TO getObj(int id) {
+        if (! this.idExist(id)) {
+            throw new Error("identificateur non existant");
+        }
+        return this.idVersObjet.get(id);
+    }
+    
+    public boolean idExist(int id) {
+        return this.idVersObjet.containsKey(id);
+    }
+    
+    public void associe(int id,TO obj) {
+        if (this.idExist(id)) {
+            throw new Error("identificateur existant");
+        }
+        this.idVersObjet.put(id, obj);
+        this.objetVersId.put(obj, id);
+    }  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
