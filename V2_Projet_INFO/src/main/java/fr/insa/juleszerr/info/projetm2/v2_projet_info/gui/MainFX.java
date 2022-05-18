a2c11f19f3bcce4d218bd0ec7d39f142a10d4c35
corin <corin@LAPTOP-KEPVCK3S>
2022-05-18 22:35:53 +0200

    zadae

--- V2_Projet_INFO/src/main/java/fr/insa/juleszerr/info/projetm2/v2_projet_info/gui/MainFX.java
+++ V2_Projet_INFO/src/main/java/fr/insa/juleszerr/info/projetm2/v2_projet_info/gui/MainFX.java
@@ -22,6 +22,23 @@ import javafx.stage.Stage;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -38,8 +55,13 @@ public class MainFX extends Application  {
         MainPane main = new MainPane(primaryStage,test);
         Scene s = new Scene(main,1000,600);
         primaryStage.resizableProperty();
+         primaryStage.setTitle("super  projet d'info");
+       primaryStage.getIcons().add( new Image("https://seeklogo.com/images/A/aperture-science-logo-57F8044379-seeklogo.com.png"));
         primaryStage.setScene(s);
+    
         primaryStage.show();
+        
+        
         
     }
 
